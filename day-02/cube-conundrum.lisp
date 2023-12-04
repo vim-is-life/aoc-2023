571e16950c1cba203d2c6342ad28e4ca060dd119
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2023-12-04 00:54:13 -0600

    day2: almost done with part 1

--- day-02/cube-conundrum.lisp
+++ day-02/cube-conundrum.lisp
@@ -4,6 +4,7 @@
 ;;;; Author: vim-is-life
 ;;;; Date: 3 Dec 2023
 (ql:quickload "str")
+(ql:quickload "alexandria")
 
 ;;; part 1: must find the games that would be possible if the bag only has
 ;;; - 12 red cubes
@@ -15,42 +16,55 @@
 ;;; of times we've seen the elf pull out a given color from his bag for a given
 ;;; game.
 
-(defun process-data-section (game-data-alist remainder-of-data)
-  "TODO doc"
-  (if remainder-of-data
-      (let ((data-section (str:split "," (car remainder-of-data))))
-        (dolist (color-datapoint data-section)
-          (destructuring-bind (number-str color-name) (str:words color-datapoint)
-            (let* ((color-data (cdr game-data-alist))
-                   (matching-color-data (assoc color-name color-data :test 'string=))
-                   (highest-occurences-yet (cdr matching-color-data)))
-              (setf (cdr matching-color-data) (max highest-occurences-yet
-                                                   (parse-integer number-str)))
-              (format t "just before maessing with game-data-alist")
-              (setf (cdr game-data-alist) (append (remove-if (lambda (alist)
-                                                               (string= (car alist)
-                                                                        color-name))
-                                                             color-data)
-                                                  matching-color-data))
-              (print matching-color-data))))
-        (format t "out of dolist")
-        (process-data-section game-data-alist (cdr remainder-of-data)))
-      game-data-alist))
+(defun process-data-section (section)
+  (destructuring-bind (cube-count-as-str cube-color) (str:words section)
+    (let* ((cube-count-as-int (parse-integer cube-count-as-str)))
+      (cons cube-color cube-count-as-int))))
 
-(defun process-data-line (line)
-  "Returns an association list that contains information about the maximum
-number of times we've seen a die for a given game id. Returned alist is of form
-(<game-id> . (\"<color>\" . <number-of-times-seen>)."
-  (destructuring-bind (id-section-str rest-after-id) (str:split ":" line)
-    ;; we use digit-char-p twice because can be used to convert or as a predicate
-    (let ((id-as-number (digit-char-p (find-if #'digit-char-p id-section-str))))
-      ;; use labels here to let us define a closure or mini function to handle problem.
-      (process-data-section (cons id-as-number '(("red"   . 0)
-                                                 ("green" . 0)
-                                                 ("blue"  . 0)))
-                            (str:split ";" rest-after-id)))))
+(defun get-all-data-after-id (draw-info)
+  "Returns an alist containing data on all occurrences of the drawing of the
+given colors."
+  (let* ((all-fields (str:split ";" draw-info))
+         (data-from-all-fields (alexandria:flatten
+                                (mapcar (lambda (field)
+                                          (mapcar #'process-data-section
+                                                  (str:split "," field)))
+                                        all-fields))))
+    (alexandria:plist-alist data-from-all-fields)))
+
+                                        ; => (("blue" . 3) ("red" . 4) ("red" . 1) ("green" . 2) ("blue" . 6) ("green" . 2))
+(get-all-data-after-id " 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+
+(defun get-draw-stats-with-max-cube-count (all-game-data-alist)
+  (labels ((get-maxes (datapoints-left-to-check maxes-so-far-plist)
+             (let* ((remaining-datapoints (cdr datapoints-left-to-check))
+                    (working-datapoint    (car datapoints-left-to-check))
+                    (color-in-question    (car working-datapoint))
+                    (number-in-question   (cdr working-datapoint)))
+               (destructuring-bind (color-1 count-1 color-2 count-2 color-3 count-3) maxes-so-far-plist
+                 (if working-datapoint
+                     (cond ((string= color-1 color-in-question)
+                            (get-maxes remaining-datapoints
+                                       (list color-1 (max number-in-question count-1)
+                                             color-2 count-2
+                                             color-3 count-3)))
+                           ((string= color-2 color-in-question)
+                            (get-maxes remaining-datapoints
+                                       (list color-1 count-1
+                                             color-2 (max number-in-question count-2)
+                                             color-3 count-3)))
+                           ((string= color-3 color-in-question)
+                            (get-maxes remaining-datapoints
+                                       (list color-1 count-1
+                                             color-2 count-2
+                                             color-3 (max number-in-question count-3)))))
+                     maxes-so-far-plist)))))
+    (get-maxes all-game-data-alist (list "red"   0
+                                         "green" 0
+                                         "blue"  0))))
 
 
-
-
-;; (process-data-line "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+(let ((my-data (get-all-data-after-id
+                " 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")))
+  (get-draw-stats-with-max-cube-count my-data))
+                                        ; => ("red" 14 "green" 3 "blue" 15)
