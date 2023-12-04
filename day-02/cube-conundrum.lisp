508f8acd6c8a5ac1c413088361a40c6d9c149789
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2023-12-04 02:21:33 -0600

    day2: solved part 1

--- day-02/cube-conundrum.lisp
+++ day-02/cube-conundrum.lisp
@@ -5,6 +5,7 @@
 ;;;; Date: 3 Dec 2023
 (ql:quickload "str")
 (ql:quickload "alexandria")
+(load "../utils.lisp")
 
 ;;; part 1: must find the games that would be possible if the bag only has
 ;;; - 12 red cubes
@@ -36,6 +37,11 @@ given colors."
 (get-all-data-after-id " 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
 
 (defun get-draw-stats-with-max-cube-count (all-game-data-alist)
+  "Returns a plist of pairs of colors and how many times those colors appeared
+in a given line based on the data for said line passed in as
+ALL-GAME-DATA-ALIST. Caller can rely on plist being returned in the form of
+(\"red\" NUMBER-OF-RED-OCCURENCES \"green\" NUMBER-OF-GREEN-OCCURENCES \"blue\"
+NUMBER-OF-BLUE-OCCURENCES)."
   (labels ((get-maxes (datapoints-left-to-check maxes-so-far-plist)
              (let* ((remaining-datapoints (cdr datapoints-left-to-check))
                     (working-datapoint    (car datapoints-left-to-check))
@@ -64,7 +70,52 @@ given colors."
                                          "blue"  0))))
 
 
-(let ((my-data (get-all-data-after-id
-                " 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")))
-  (get-draw-stats-with-max-cube-count my-data))
+;; (let ((my-data (get-all-data-after-id
+;;                 " 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")))
+;;   (get-draw-stats-with-max-cube-count my-data))
                                         ; => ("red" 14 "green" 3 "blue" 15)
+
+(defparameter *max-red*   12)
+(defparameter *max-green* 13)
+(defparameter *max-blue*  14)
+
+(defun is-a-possible-game-p (game-data red-capacity green-capacity blue-capacity)
+  "Returns true if game data has less occurrences of red, green, and blue than
+RED-CAPACITY, GREEN-CAPACITY, and BLUE-CAPACITY, respectively. Input is
+expected to be in the form
+(GAME-ID (\"red\" RED-OCCURRENCES \"green\" GREEN-OCCURRENCES \"blue\" BLUE-OCCURRENCES))."
+  (let ((data-without-id (cadr game-data)))
+    (destructuring-bind (_ red-seen _ green-seen _ blue-seen) data-without-id
+      (and (<= red-seen red-capacity)
+           (<= green-seen green-capacity)
+           (<= blue-seen blue-capacity)))))
+
+
+(defun solve-part-one ()
+  (let* ((input-file "./input-p1-p2.txt")
+         (file-lines (get-file-contents input-file))
+         ;; all game stats is list of game id and stats of form (GAME-ID (<STATS>)),
+         ;; see docs in GET-DRAW-STATS-WITH-MAX-CUBE-COUNT for what format of
+         ;; format of stats is
+         (all-game-stats (mapcar (lambda (line)
+                                   (destructuring-bind (game-id game-information) (str:split ":" line)
+                                     (let* ((game-data (get-all-data-after-id game-information))
+                                            (color-occurrence-stats (get-draw-stats-with-max-cube-count game-data)))
+                                       ;; word "Game" is 4 letters long + 1 space, index is 1 less
+                                       (list (parse-integer game-id :start 4)
+                                             color-occurrence-stats))))
+                                 file-lines))
+         (sum-of-possible-game-ids (reduce (lambda (first-it second-it)
+                                             (+ (if (numberp first-it)
+                                                    first-it
+                                                    (if (is-a-possible-game-p first-it *max-red* *max-green* *max-blue*)
+                                                        (car first-it)
+                                                        0))
+                                                (if (is-a-possible-game-p second-it *max-red* *max-green* *max-blue*)
+                                                    (car second-it)
+                                                    0)))
+                                           all-game-stats)))
+
+    sum-of-possible-game-ids))
+
+(print (solve-part-one))
