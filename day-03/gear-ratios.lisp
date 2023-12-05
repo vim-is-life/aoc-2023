d5480b080f026145c7819150f28e66ae6c6f810a
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2023-12-05 05:58:28 -0600

    day3: got code working nearly perfect
    i almost have the correct answer but i'm getting one over it. i'll have
    to see if i'm missing accounting for overlap somehow

--- day-03/gear-ratios.lisp
+++ day-03/gear-ratios.lisp
@@ -5,6 +5,7 @@
 ;;;; Date: 4 Dec 2023
 (ql:quickload "str")
 (ql:quickload "cl-ppcre")
+(load "../utils.lisp")
 
 (defparameter *symbol-regex* (ppcre:create-scanner  "[~!@#$%^&*-+=-]")
   "The regex scanner to identify symbol characters in the input.")
@@ -34,57 +35,103 @@ in the line they were searched for in according to SEARCH-RESULTS."
                  accum)))
     (recursive-reshaper nil search-results)))
 
-(defun get-numbers-and-symbols-in-line (line)
-  (list (reshape-results (ppcre:all-matches *number-regex* line))
-        (reshape-results (ppcre:all-matches *symbol-regex* line) t)))
+(defun get-numbers-in-line (line)
+  (reshape-results (ppcre:all-matches *number-regex* line)))
 
-(destructuring-bind (numbers symbols) (get-numbers-and-symbols-in-line
-                                       ".....*...=.875*342......$........................@........-.....846.........610..829.934*508...79............&691..48...................483.")
-  (print numbers)
-  (print symbols))
+(defun get-symbols-in-line (line)
+  (reshape-results (ppcre:all-matches *symbol-regex* line) t))
 
 (defun number-adjacent-p (number-start-and-end-indices symbol-index)
-  (((labels ((number-index-adjacent-p (number-index symbol-index)
-               (destructuring-bind (number-x number-y symbol-x symbol-y)
-                   (append number-index symbol-index)
-                 ;; number is directly right of symbol
-                 (or (and (= number-y symbol-y)
-                          (= (1+ number-x) symbol-x))
-                     ;; number is directly left of symbol
-                     (and (= number-y symbol-y)
-                          (= (1- number-x) symbol-x))
-                     ;; number is directly above (ie row - 1) of symbol
-                     (and (= (1- number-y) symbol-y)
-                          (= number-x symbol-x))
-                     ;; number is directly below (ie row + 1) of symbol
-                     (and (= (1+ number-y) symbol-y)
-                          (= number-x symbol-x))
-                     ;; number is diagonal down-right
-                     (and (= (1+ number-y) symbol-y)
-                          (= (1+ number-x) symbol-x))
-                     ;; number is diagonal down-left
-                     (and (= (1+ number-y) symbol-y)
-                          (= (1- number-x) symbol-x))
-                     ;; number is diagonal up-right
-                     (and (= (1- number-y) symbol-y)
-                          (= (1+ number-x) symbol-x))
-                     ;; number is diagonal up-left
-                     (and (= (1- number-y) symbol-y)
-                          (= (1- number-x) symbol-x))))))
-      (destructuring-bind (number-start-index number-end-index)
-          (or (number-start-index symbol-index)
-              (number-end-index symbol-index)))))))
+  (labels ((either-side-adjacent-p (number-idx symbol-idx)
+             ;; number is to right
+             (or (= (1+ number-idx) symbol-idx)
+                 ;; number is to left
+                 (= (1- number-idx) symbol-idx)
+                 ;; number is above or below
+                 (= number-idx symbol-idx))))
+    (destructuring-bind (number-start-index number-end-index) number-start-and-end-indices
+      (or (either-side-adjacent-p number-start-index symbol-index)
+          (either-side-adjacent-p number-end-index symbol-index)))))
 
+(defun not-adjacent-p (number-index symbol-indices)
+  (notany (lambda (symbol-index)
+            (number-adjacent-p number-index symbol-index))
+          symbol-indices))
 
-(defun idkman (multiline-str)
-  (let ((line-length (get-line-length multiline-str)))
-    (labels ((check-for-syms-around-nums (accum last-line-info this-line-info next-line-info rest-of-lines)
-               (if rest-of-lines
-                   accum
-                   ;; for every line, find numbers (list of (start end)) and symbols.
-                   ;; for each (start end) elem in number list, collect the number if it is adjacent to a symbol
-                   (let ((new-next-line-info (list ())))
-                     (check-for-syms-around-nums (cons new-results accum)
-                                                 this-line-info next-line-info
-                                                 new-next-line-info
-                                                 (cdr rest-of-lines)))))))))
+(defun get-adjacent-numbers-from-3-lines (line-1-number-indices
+                                          line-2-number-indices
+                                          line-3-number-indices
+                                          symbol-indices)
+  (let ((adjacent-from-1 (remove-if (lambda (num-idx)
+                                      (not-adjacent-p num-idx symbol-indices))
+                                    line-1-number-indices))
+        (adjacent-from-2 (remove-if (lambda (num-idx)
+                                      (not-adjacent-p num-idx symbol-indices))
+                                    line-2-number-indices))
+        (adjacent-from-3 (remove-if (lambda (num-idx)
+                                      (not-adjacent-p num-idx symbol-indices))
+                                    line-3-number-indices)))
+    (union adjacent-from-1 (union adjacent-from-2 adjacent-from-3))))
+
+(defun get-number-from-index (start-end-index-pair candidate-lines)
+  (destructuring-bind (idx _ line-1 line-2 line-3) (append start-end-index-pair candidate-lines)
+    (some (lambda (x) (if x x)) (list (parse-integer line-1 :junk-allowed t :start idx)
+                                      (if line-2 (parse-integer line-2 :junk-allowed t :start idx))
+                                      (if line-3 (parse-integer line-3 :junk-allowed t :start idx))))))
+
+(defun get-numbers-from-indices (number-indices candidate-lines)
+  (labels ((collect-numbers (accum number-indices lines)
+             (if (not number-indices)
+                 accum
+                 (let ((number (get-number-from-index (car number-indices) lines)))
+                   (print number)
+                   (collect-numbers (cons number accum) (cdr number-indices) lines)))))
+    (collect-numbers nil number-indices candidate-lines)))
+
+(defun get-indices-of-numbers-adjacent-to-symbols (lines)
+  (labels ((check-for-syms-around-nums (accum
+                                        numbers-in-last-line
+                                        current-line-numbers
+                                        current-line-symbols
+                                        numbers-in-next-line
+                                        rest-of-lines)
+             (if (not rest-of-lines)
+                 accum
+                 ;; for every line, find numbers (list of (start end)) and symbols.
+                 ;; for each (start end) elem in number list, collect the number if it is adjacent to a symbol
+                 ;; we're only ever checking off the symbols in the middle line in every triplet of lines
+                 (let* ((last-line    (car   rest-of-lines))
+                        (current-line (cadr  rest-of-lines))
+                        (next-line    (caddr rest-of-lines))
+                        (numbers-in-line-after-next (get-numbers-in-line (cadddr rest-of-lines)))
+                        (adjacent-indices           (get-adjacent-numbers-from-3-lines numbers-in-last-line
+                                                                                       current-line-numbers
+                                                                                       numbers-in-next-line
+                                                                                       current-line-symbols))
+                        (adjacent-numbers (get-numbers-from-indices adjacent-indices
+                                                                    (list last-line current-line next-line))))
+                   (check-for-syms-around-nums (append adjacent-numbers accum)
+                                               current-line-numbers ; make this line last line
+                                               numbers-in-next-line ; make next line this line
+                                               (get-symbols-in-line (car rest-of-lines))
+                                               numbers-in-line-after-next ; make line after next the new next
+                                               (cdr rest-of-lines))))))
+    (let* ((first-line-numbers  (get-numbers-in-line (car   lines)))
+           (second-line-numbers (get-numbers-in-line (cadr  lines)))
+           (second-line-symbols (get-symbols-in-line (cadr  lines)))
+           (third-line-numbers  (get-numbers-in-line (caddr lines))))
+      (check-for-syms-around-nums nil
+                                  first-line-numbers
+                                  second-line-numbers
+                                  second-line-symbols
+                                  third-line-numbers
+                                  lines))))
+
+                                        ; => ((2 4) (6 8) (2 3) (0 2) (6 8) (0 2) (2 3))
+
+(defun solve-part-one ()
+  (let* ((file-lines (get-file-contents "./input-p1-ex.txt"))
+         (adjacent-numbers (get-indices-of-numbers-adjacent-to-symbols file-lines)))
+    (reduce '+ adjacent-numbers)))
+
+(solve-part-one)
