c8ab8d19bc0039a83a14acd0a8fc7a683e341026
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2024-06-30 19:02:38 -0500

    day3: finished part 1!

--- day-03/gear-ratios-try-2.lisp
+++ day-03/gear-ratios-try-2.lisp
@@ -6,7 +6,7 @@
 (load "../utils.lisp")
 
 (defconstant +p1-example-input+ (coerce (get-file-contents "input-p1-ex.txt") 'vector))
-(defconstant +real-input+ (coerce (get-file-contents "input-p1-ex.txt") 'vector))
+(defconstant +real-input+ (coerce (get-file-contents "input-p1-p2.txt") 'vector))
 
 (defun clamp-index (idx lower upper)
   "Return IDX if it is within UPPER and LOWER. If IDX is below LOWER, then
@@ -23,7 +23,7 @@ return LOWER, or if IDX is above or equal to UPPER, return (1- UPPER)."
 
 (defun is-char-symbol? (charac)
   "Return non-nil if CHARAC is in a list of symbols, otherwise return nil"
-  (find charac "[~!@#$%^&*-+=-]"))
+  (find charac "[~!\/@#$%^&*-+=-]"))
 
 (defun symbol-nearby-p (line-idx char-idx input-lines)
   "Return true if there is a symbol nearby adjacent to the character at the given index pair."
@@ -59,20 +59,35 @@ problem"
         :for current-char = (aref current-line char-idx)
         :with adjacent? = nil
         :with accum = 0 :do
-          (if (numberp current-char)
+          (if (digit-char-p current-char)
               (progn
-                (incf accum (parse-integer current-char))
+                ;; have to shift all values left by one then add 
+                (setf accum (+ (parse-integer (string current-char))
+                               (* 10 accum)))
+                (format t "~%line: ~A  char-idx: ~A  accum: ~A" line-idx char-idx accum)
                 (if (symbol-nearby-p line-idx char-idx problem-input-lines)
                     (setf adjacent? t)))
-              ;; we've finished with the current number, so either update total if appropriate
+              ;; we've finished with the current number, so update total if appropriate
               (if (> accum 0)
-                  (if adjacent? (incf grand-total accum))
-                  (setf accum 0))))))
-;;; NOTE: start here when come back.
-;;; the above should work, but when i run it it reports zero. i'd be interested
-;;; to know what the nonzero values of accum are on every non number. i
-;;; basically need to see how this is actually working. i'm thinking maybe
-;;; something could be going on funky with the scope of the GRAND-TOTAL variable
-;;; so that might also need fixing.
+                  (progn
+                    (format t "~%line: ~A char-idx: ~A  accum: ~A  grand-total: ~A"
+                            line-idx char-idx accum grand-total)
+                    (if adjacent?
+                        (progn
+                          (incf grand-total accum)
+                          (setf accum     0
+                                adjacent? nil)
+                          (format t "~%~4Tline: ~A char-idx: ~A  adjacent?: ~A  grand-total: ~A"
+                                  line-idx char-idx adjacent? grand-total))
+                        (setf accum 0)))))
+        :finally (if (> accum 0)
+                     (progn
+                       (format t "~%line: ~A char-idx: ~A (end)  accum: ~A  grand-total: ~A"
+                               line-idx char-idx accum grand-total)
+                       (if adjacent?
+                           (incf grand-total accum)))))))
 
-(gear! +p1-example-input+)
+
+
+
+(gear! +real-input+)                    ; => 525911
