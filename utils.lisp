6dc6801e46949c10722ae429cf86e46e30a16f9c
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2024-07-07 20:06:32 -0500

    implemented queue adt

--- utils.lisp
+++ utils.lisp
@@ -5,3 +5,33 @@
 (defun curry (function &rest args)
   (lambda (&rest more-args)
     (apply function (append args more-args))))
+
+;;; QUEUE ADT IMPLEMENTATION
+(defclass queue ()
+  ((head :initform nil :accessor qhead)))
+
+(defmethod print-object ((q queue) stream)
+  "Print representation of queue Q to STREAM."
+  (format stream "#<QUEUE with items: (~{ ~A ~})>" (qhead q)))
+
+(defmethod qempty? ((q queue))
+  "Return non-nil if queue is empty, nil otherwise"
+  (null (qhead q)))
+
+(defmethod enqueue ((q queue) item)
+  "Add ITEM to the end of the the queue Q"
+  (with-slots (head) q
+    (if (qempty? q)
+        (setf head (list item))
+        (nconc head (list item)))))
+
+(defmethod dequeue ((q queue))
+  (with-slots (head) q
+    (unless (null head)
+      (let ((retval (first head)))
+        (setf head (rest head))
+        retval))))
+
+(defmethod qclear ((q queue))
+  (loop :while (not (qempty? q))
+        :do (dequeue q)))
