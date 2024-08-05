377611422831065274a43778b535342c2f17eae2
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2024-08-05 08:27:16 -0500

    [day11] changed to a list of lists instead

--- day-11/cosmic-expansion.lisp
+++ day-11/cosmic-expansion.lisp
@@ -1,30 +1,26 @@
 (in-package :aoc-2023)
 
 (defun custom-read (filename)
-  "Read in the problem input where every galaxy is read in as T and each
-non-galaxy space is read in as NIL. Returns an array of rank 2"
-  (let* ((file-contents (get-file-contents-vec filename))
-         (num-lines   (length file-contents))
-         (num-columns (length (aref file-contents 0))))
-    (loop
-      :with arr = (make-array `(,num-lines ,num-columns) :initial-element nil)
-      :for line :below num-lines
-      :do (loop
-            :for col :below num-columns
-            :for cur-char = (-> file-contents (aref line) (aref col))
-            :when (eql cur-char #\#)
-              :do (setf (aref arr line col) t))
-      :finally (return arr))))
+  (let ((file-contents (get-file-contents filename)))
+    (mapcar (lambda (line)
+              (map 'list
+                   (lambda (char)
+                     (if (eql char #\#) 'gal nil))
+                   line))
+            file-contents)))
+
+;; (custom-read "example-input.txt")
 
 (defconstant +d11/example-input+ (custom-read "example-input.txt"))
 (defconstant +d11/puzzle-input+  (custom-read "puzzle-input.txt"))
 
 ;;; PART 1
-(defun num-galaxies (input-arr)
+(defun num-galaxies (input)
   "Return the number of galaxies in the universe that INPUT represents"
-  (destructuring-bind (lines cols) (array-dimensions input-arr)
-    (let ((flat-input (make-array (* lines cols) :displaced-to input-arr)))
-      (count t flat-input))))
+  (->> input
+    (mapcar (lambda (line)
+              (count 'gal line)))
+    (reduce #'+)))
 
 ;; (num-galaxies +d11/example-input+)
 
