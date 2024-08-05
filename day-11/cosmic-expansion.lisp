365a869795ab439f1469b706f045901973989992
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2024-08-05 09:10:01 -0500

    [day11] got universe expansion working right

--- day-11/cosmic-expansion.lisp
+++ day-11/cosmic-expansion.lisp
@@ -30,6 +30,74 @@
 
 ;; (num-pairs +d11/example-input+)
 
+(defun list-of-lists-as-string (list-of-lists)
+  (->> list-of-lists
+    (mapcar (lambda (row)
+              (map 'string
+                   (lambda (spc) (if (eql spc 'gal) #\# #\.))
+                   row)))
+    (format nil "~{~A~%~}")))
+
+;; (list-of-lists-as-string +d11/example-input+)
+
+(defun transpose (list-of-lists)
+  "Return the tranpose of LIST-OF-LISTS, where LIST-OF-LISTS is a list of lists
+of symbols."
+  ;; taken from 2-3 of Common Lisp Recipes book
+  (apply #'mapcar #'list list-of-lists))
+
+;; (transpose +d11/example-input+)
+
+(defun expand-universe (input)
+  "Return the expanded universe or expanded version of INPUT where all empty
+rows and columns are doubled (1 row -> 2 rows, and same for columns).
+Note that the columns and rows will come back reversed."
+  (labels ((double-rows (rows acc)
+             (if rows
+                 (let ((row (first rows))
+                       (rest-rows (rest rows)))
+                   (if (every #'null row)
+                       ;; need to double the row that has all empties
+                       (double-rows rest-rows (cons row (cons row acc)))
+                       (double-rows rest-rows (cons row acc))))
+                 acc)))
+    (-> input
+      ;; first pass: do rows
+      (double-rows '())
+      ;; then have to transpose to do columns, then re-transpose
+      transpose
+      (double-rows '())
+      transpose)))
+
+;; (expand-universe +d11/example-input+)
+;; (expand-universe +d11/puzzle-input+)
+
+;; this is really only to test that i have the first function right
+(defun expand-universe-inorder (input)
+  "Return the expanded universe or expanded version of INPUT where all empty
+rows and columns are doubled (1 row -> 2 rows, and same for columns)."
+  (labels ((double-rows (rows acc)
+             (if rows
+                 (let ((row (first rows))
+                       (rest-rows (rest rows)))
+                   (if (every #'null row)
+                       ;; need to double the row that has all empties
+                       (double-rows rest-rows (cons row (cons row acc)))
+                       (double-rows rest-rows (cons row acc))))
+                 acc)))
+    (-> input
+      ;; first pass: do rows
+      (double-rows '())
+      ;; then have to transpose to do columns, then re-transpose
+      reverse
+      transpose
+      (double-rows '())
+      reverse
+      transpose)))
+
+(assert (equal (expand-universe-inorder +d11/example-input+)
+               (custom-read "example-expansion.txt")))
+
 (defun d11/part-1 (input)
   -1)
 
